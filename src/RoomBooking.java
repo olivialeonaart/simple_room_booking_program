13cea00cde1ee351a9315b1179f0d0d219b8cc99
olivialeonaart <olivialeonaart@gmail.com>
2025-11-08 22:30:25 -0500

    8 November, 2025

--- src/RoomBooking.java
+++ src/RoomBooking.java
@@ -12,100 +12,164 @@ import java.util.Scanner;
 
 
 public class RoomBooking {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+        /**
+         * step two - declare our room types and variables
+         */
 
-    /**
-     * step two - declare our room types and variables
-     */
+        String occupancychoice = "";
+        String occupancytype1 = "Single";
+        String occupancytype2 = "Double";
+        String occupancytype3 = "Triple";
 
-    String occupancychoice;
-    String occupancytype1 = "Single";
-    String occupancytype2 = "Double";
-    String occupancytype3 = "Triple";
+        String bedconfigchoice = "";
+        String bedconfig_full = "Full";
+        String bedconfig_queen = "Queen";
+        String bedconfig_king = "King";
 
-    String bedconfigchoice;
-    String bedconfig_full = "Full";
-    String bedconfig_queen = "Queen";
-    String bedconfig_king = "King";
+        String layoutchoice = "";
+        String layout_standard = "Standard";
+        String layout_deluxe = "Deluxe";
+        String layout_suite = "Suite";
 
-    String layoutchoice;
-    String layout_standard = "Standard";
-    String layout_deluxe = "Deluxe";
-    String layout_suite = "Suite";
+        String specialfeatureschoice1;
+        String specialfeatureschoice2;
+        String specialfeatureschoice3;
+        String specialfeatureschoice4;
+        String specialfeatures_accessible = "Handicap Accessible";
+        String specialfeatures_petfriendly = "Pet Friendly";
+        String specialfeatures_nonsmoking = "Non-Smoking";
+        String specialfeatures_excecutive = "Executive Upgrade";
 
-    String specialfeatureschoice1;
-    String specialfeatureschoice2;
-    String specialfeatureschoice3;
-    String specialfeatureschoice4;
-    String specialfeatures_accessible = "Handicap Accessible";
-    String specialfeatures_petfriendly = "Pet Friendly";
-    String specialfeatures_nonsmoking = "Non-Smoking";
-    String specialfeatures_excecutive = "Executive Upgrade";
+        /**
+         * step three - show our menus and get input
+         */
+        String linebreak = System.lineSeparator();
+        System.out.println("Simple Room Booking Service: " + linebreak + linebreak);
+        System.out.println("Please enter the number corresponding to each menu option" + linebreak + linebreak);
+        //menu 1
+        System.out.println("1. Book a Room");
+        System.out.println("2. Exit Program");
 
-    /**
-     * step three - show our menus and get input
-     */
-    String linebreak = System.lineSeparator();
-    System.out.println("Simple Room Booking Service: " + linebreak + linebreak);
-    System.out.println("Please enter the number corresponding to each menu option" + linebreak + linebreak);
+        System.out.print("Choice : ");
+        int choice_start = scanner.nextInt();
 
-    System.out.println("1. Book a Room");
-    System.out.println("2. Exit Program");
+        //menu 2
+        if (choice_start == 1) {
+            System.out.println("Welcome to your Room Booking Service" + linebreak +
+                    "Please select your occupancy type: " + linebreak);
 
-    System.out.println("Choice : ");
-    int choice_start = scanner.nextInt();
-
-    if (choice_start == 1) {
-        System.out.println("Welcome to your Room Booking Service" + linebreak +
-                "Please select your occupancy type: " + linebreak);
-
-        System.out.println("1. Single");
-        System.out.println("2. Double");
-        System.out.println("1. Triple (or more)");
-        System.out.println("4. EXIT Program");
-
-        System.out.println("Choice: ");
-        int choice_occupancy = scanner.nextInt();
-
-        if (choice_occupancy == 1) {
-            occupancychoice = "Single";
-
-            //next menu (bed config) here
-            System.out.println(linebreak + "Please select your preferred bed size: " + linebreak);
-
-            System.out.println("1. Full");
-            System.out.println("2. Queen");
-            System.out.println("3. King");
+            System.out.println("1. Single");
+            System.out.println("2. Double");
+            System.out.println("1. Triple (or more)");
             System.out.println("4. EXIT Program");
 
-            System.out.println("Choice: ");
-            int choice_bedconfig = scanner.nextInt();
+            System.out.print("Choice: ");
+            int choice_occupancy = scanner.nextInt();
+
+            if (choice_occupancy == 1) {
+                occupancychoice = occupancytype1;
+            } else if (choice_occupancy == 2) {
+                occupancychoice = occupancytype2;
+            } else if (choice_occupancy == 3) {
+                occupancychoice = occupancytype3;
+            } else if (choice_occupancy == 4) {
+                System.exit(0);
+            } else {
+                System.out.print(linebreak + "Please input choices numbered 1 through 4 or manually exit the program: ");
+            }
+
+            }
+        //menu 3
+        System.out.println(linebreak + "Please select your preferred bed size: " + linebreak);
+
+        System.out.println("1. Full");
+        System.out.println("2. Queen");
+        System.out.println("3. King");
+        System.out.println("4. EXIT Program");
+
+        System.out.print("Choice: ");
+        int choice_bedconfig = scanner.nextInt();
+
+        if (choice_bedconfig == 1) {
+            bedconfigchoice = bedconfig_full;
         }
-        else if (choice_occupancy == 2) {
-            occupancychoice = "Double";
-
-            //next menu here
+        else if (choice_bedconfig == 2) {
+            bedconfigchoice = bedconfig_queen;
+        }
+        else if (choice_bedconfig == 3) {
+            bedconfigchoice = bedconfig_king;
+        }
+        else if (choice_bedconfig == 4) {
+            System.exit(0);
 
         }
-        else if (choice_occupancy == 3) {
-            occupancychoice = "Triple";
-
-            //next menu here
-
+        else {
+            System.out.print(linebreak + "Please input choices numbered 1 through 4 or manually exit the program: ");
         }
-        else if (choice_occupancy == 4) {
+
+        //menu 4
+        System.out.println(linebreak + "Please select your preferred room layout: " + linebreak);
+
+        System.out.println("1. Standard");
+        System.out.println("2. Deluxe");
+        System.out.println("3. Suite");
+        System.out.println("4. EXIT Program");
+
+        System.out.print("Choice: ");
+        int choice_roomlayout = scanner.nextInt();
+
+        if (choice_roomlayout == 1) {
+            layoutchoice = layout_standard;
+        }
+        else if (choice_roomlayout == 2) {
+            layoutchoice = layout_deluxe;
+        }
+        else if (choice_roomlayout == 3) {
+            layoutchoice = layout_suite;
+        }
+        else if (choice_roomlayout == 4) {
             System.exit(0);
         }
         else {
-            System.out.println(linebreak + "Please input choices numbered 1 through 4 or manually exit the program: ");
+            System.out.print(linebreak + "Please input choices numbered 1 through 4 or manually exit the program: ");
+        }
+
+        //menu 5 y/n
+        System.out.println(linebreak + "Would you like to hear about our offered accessibility features? " + linebreak);
+
+        System.out.println("1. Yes");
+        System.out.println("2. No");
+        System.out.println("3. EXIT");
+
+        System.out.print("Choice: ");
+        int choice_specialfeaturesmenu = scanner.nextInt();
+
+        if (choice_specialfeaturesmenu == 1) {
+            //next menu
+        }
+        else if (choice_specialfeaturesmenu == 2) {
+            //results display
+            System.out.println(linebreak + "Thank you for choosing our room booking service!");
+            System.out.println(linebreak + "Here are your selected preferences: " + linebreak);
+
+            System.out.println("Occupancy Type: " + occupancychoice);
+            System.out.println("Bed Configuration: " + bedconfigchoice);
+            System.out.println("Room Layout: " + layoutchoice);
+            System.out.println("Accessibility Features: None" + linebreak);
+
+            System.out.println("The booking system will now close, press any key to EXIT PROGRAM");
+            int end_program = scanner.nextInt();
+                System.exit(0);
+            }
+        else if (choice_specialfeaturesmenu == 3) {
+            System.exit(0);
+        }
+        }
+
         }
 
 
-    }
 
-
-}
-
-}
